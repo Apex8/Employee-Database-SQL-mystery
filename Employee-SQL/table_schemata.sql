a602049cc35697cf901b9480ae391e4216503ef7
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-08-11 14:21:34 -0400

    edit

--- Employee-SQL/table_schemata.sql
+++ Employee-SQL/table_schemata.sql
@@ -1,7 +1,6 @@
--- Drop table if exists
+
 DROP TABLE IF EXISTS departments;
 
--- Create new table
 CREATE TABLE departments (
 	dept_no 		VARCHAR(4) 		NOT NULL,
 	dept_name 		VARCHAR(40)		NOT NULL,
@@ -9,10 +8,9 @@ CREATE TABLE departments (
 	PRIMARY KEY (dept_no)
 );
 
--- Drop table if exists
+
 DROP TABLE IF EXISTS employees;
 
--- Create new table
 CREATE TABLE employees (
 	emp_no 			INT 		NOT NULL,
 	emp_title 		VARCHAR 	NOT NULL,
@@ -24,10 +22,9 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 
--- Drop table if exists
+
 DROP TABLE IF EXISTS dept_emp;
 
--- Create new table
 CREATE TABLE dept_emp (
 	emp_no 			INT 			NOT NULL,
 	dept_no 		VARCHAR(4) 		NOT NULL,
@@ -37,49 +34,29 @@ CREATE TABLE dept_emp (
 );
 
 
--- Drop table if exists
 DROP TABLE IF EXISTS dept_manager;
 
--- Create new table
 CREATE TABLE dept_manager (
-	dept_no 		VARCHAR,
-	emp_no 			INT
+	dept_no 		VARCHAR(4)	NOT NULL,
+	emp_no 			INT			NOT NULL
+	PRIMARY KEY (emp_no,dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES employees (dept_no)
 );
 
--- View table columns and datatypes
-SELECT * FROM dept_manager;
 
-
-
-
-
-
-
-
-
--- Drop table if exists
 DROP TABLE IF EXISTS salaries;
 
--- Create new table
 CREATE TABLE salaries (
-	emp_no INT,
-	salary INT
+	emp_no INT		NOT NULL,
+	salary INT		NOT NULL
+	PRIMARY KEY (emp_no)	
 );
 
--- View table columns and datatypes
-SELECT * FROM salaries;
 
-
-
-
--- Drop table if exists
 DROP TABLE IF EXISTS titles;
 
--- Create new table
 CREATE TABLE titles (
 	title_id VARCHAR,
 	title VARCHAR
 );
-
--- View table columns and datatypes
-SELECT * FROM titles;
