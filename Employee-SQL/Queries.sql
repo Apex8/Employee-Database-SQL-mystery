640f8eb08fb564df4836788ee6cb8e53b1b294cb
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-08-17 19:50:12 -0400

    final commit

--- Employee-SQL/Queries.sql
+++ Employee-SQL/Queries.sql
@@ -33,7 +33,7 @@ FROM employees WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp JOIN employees ON dept_emp.emp_no = employees.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no WHERE departments.dept_name = 'Sales';
-	
+
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
